afbac6f652f008c7176a8188b6093e38b8607b6d
eutteum <ket5238@amtel.co.kr>
2021-04-30 14:25:47 +0900

    practice

--- basic/packing_unpacking.py
+++ basic/packing_unpacking.py
@@ -7,3 +7,12 @@ print(c)
 
 d, e = c
 print(d,e)
+
+numbers = [ (1,2),(10,0) ]
+
+for a,b in numbers:
+    if b == 0:
+        print("0으로 나눌 수는 없습니다.")
+    else:
+        continue
+        print("{}를 {}로 나누면 {}".format(a,b,a/b))
