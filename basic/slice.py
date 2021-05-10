734e8e497a884250c6be16891bba6f08ec5747ba
eutteum <ket5238@amtel.co.kr>
2021-05-10 18:26:01 +0900

    practice slice

---
+++ basic/slice.py
@@ -0,0 +1,25 @@
+list = [1,2,3,4,5]
+
+text = "hello world"
+
+print(text[1:5])
+
+list = ['영','일','이','삼','사','오']
+
+print(list[1:3])
+"""
+list1 = list
+
+print(list1)
+
+list.sort()
+
+print(list1)
+"""
+list2 = list[:]
+
+print(list2)
+
+list.sort()
+
+print(list2)
