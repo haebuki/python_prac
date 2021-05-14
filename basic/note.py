c995a8bcf7d25356cabf75db719796e7d038e92b
eutteum <ket5238@amtel.co.kr>
2021-05-14 13:14:03 +0900

    practice

--- basic/note.py
+++ basic/note.py
@@ -1,6 +1,14 @@
-import collections
+mul=1
+result=False
+for a in range(5):
+    x = int(input())
+    mul = mul*x
+    for i in range(100):
+        if mul==i**2:
+            result=True
+            break
 
-my_list = [1,2,3,4,5,6,7,8,7,9,1,2,3,3,44,2,6,4,3,6,3]
-answer = collections.Counter(my_list)
-
-print(type(answer))
+if result !=True:
+    print("not found")
+else:
+    print("found")
