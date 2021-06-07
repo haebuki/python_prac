b9858e16c95f043dc09ce2dcd339385cc6efd5d2
eutteum <ket5238@amtel.co.kr>
2021-06-07 17:16:58 +0900

    #sw academy 특별한 정렬 (History에 모든 문제풀이 있음)

--- basic/note.py
+++ basic/note.py
@@ -1,43 +1,27 @@
 #
-#  topic : 이진 탐색
+#  topic : 특별한 정렬
 '''
  description : sw expert academy list2 problem
- reference!
+ reference! split 함수는 split(" ")와 split()의 의미는 같지만 실행속도에 대한 차이가 있따.
+ 아무래도 모든 요소를 반복하며 스페이스를 찾고 나누는 작업이 있기때문에 훨씬 느린게 아닐까 싶다..
+ 저거떄문에 너무 많은 런타임 에러를 겪엇다... ㅠㅠ 앞으로는 자동으로 스플릿될 수 있는 상황이면 split()원형을 쓰자..
 '''
 #  @author haebuki
 #  @date 2021-05-28
 #  @version None 작업 내용 None
 #  @see None
 
-test_case = int(input())
+testcase = int(input())
 
-for tc in range(1,test_case+1):
-    result = 0
-    search = 0
-    P,Pa,Pb = map(int,input().split(" "))
-    start = [1, 1]
-    end = [P, P]
-    while 1:
-        search = [int((start[0]+end[0])/2),int((start[1]+end[1])/2)]
-
-        if search[0] == Pa and search[1] == Pb:
-            result = '0'
-            break
-        elif search[0] == Pa:
-            result = 'A'
-            break
-        elif search[1] == Pb:
-            result = 'B'
-            break
-
-        if Pa > search[0]:
-            start[0] = search[0]
-        elif Pa < search[0]:
-            end[0] = search[0]
-        if Pb > search[1]:
-            start[1] = search[1]
-        elif Pb < search[1]:
-            end[1] = search[1]
-
-    print("#{} {}".format(tc,result))
-
+for tc in range(1, testcase + 1):
+    N = input()
+    aj = list(map(int, input().split()))
+    sorted_list = []
+    aj.sort()
+    for i in range(0,10):
+        if i % 2 == 0 :
+            sorted_list.append(aj.pop())
+        else:
+            sorted_list.append(aj.pop(0))
+    print("#{} ".format(tc), end="")
+    print(*sorted_list)
