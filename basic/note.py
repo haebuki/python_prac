ce28bfdab6b75f0b636b193fc93cf4c038ccfef6
eutteum <ket5238@amtel.co.kr>
2021-06-25 16:36:02 +0900

    #sw academy 종이붙이기 (History에 모든 문제풀이 있음)

--- basic/note.py
+++ basic/note.py
@@ -1,5 +1,5 @@
 #
-#  topic : 글자수
+#  topic : 종이붙이기
 #  description : SW expert academy [string 학습] 문제 풀이
 #
 #  @author haebuki
@@ -7,16 +7,18 @@
 #  @version None 작업 내용 None
 #  @see None
 #
+f = [0 for _ in range(100)]
 test_case = int(input())
 
 for tc in range(1,test_case+1):
-    str1 = list(input())
-    str2 = input()
+    result=0
+    f[0]=1
+    f[1]=3
+    N = int(int(input())/10)
 
-    max = 0
+    for n in range(2,N+1):
+        if f[n]==0:
+            f[n]=(2*f[n-2]+f[n-1])
 
-    for i in str1:
-        if str2.count(i) > max:
-            max = str2.count(i)
 
-    print("#{} {}".format(tc, max))
+    print("#{} {}".format(tc, f[N-1]))
