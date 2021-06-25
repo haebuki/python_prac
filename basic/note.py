2f11d6fc53828581ae7e8485983f82802f0aee4c
eutteum <ket5238@amtel.co.kr>
2021-06-25 13:48:53 +0900

    #sw academy 글자수 (History에 모든 문제풀이 있음)

--- basic/note.py
+++ basic/note.py
@@ -1,34 +1,22 @@
+#
+#  topic : 글자수
+#  description : SW expert academy [string 학습] 문제 풀이
+#
+#  @author haebuki
+#  @date 2021-06-25
+#  @version None 작업 내용 None
+#  @see None
+#
 test_case = int(input())
 
 for tc in range(1,test_case+1):
-    N, M = map(int, input().split())
-    map1=[]
-    map2=[]
-    end_flag=0
-    result=[]
+    str1 = list(input())
+    str2 = input()
 
-    #create map and reversed map
-    for i in range(N):
-        map1.append(list(input()))
-    map2=list(zip(*map1))
+    max = 0
 
-    for i in range(N):
-        index=0
-        while True:
-            if map1[i][index:index+M] == list(reversed(map1[i][index:index+M])):
-                result = map1[i][index:index+M]
-                end_flag = 1
-                break
-            elif map2[i][index:index+M] == tuple(reversed(map2[i][index:index+M])):
-                result = map2[i][index:index + M]
-                end_flag = 1
-                break
-            else:
-                index+=1
+    for i in str1:
+        if str2.count(i) > max:
+            max = str2.count(i)
 
-            if index+M > N:
-                break
-    if end_flag==1:
-        print("#{} {}".format(tc,"".join(result)))
-
-
+    print("#{} {}".format(tc, max))
