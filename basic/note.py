ec499c6259f659580c235b2ff80be1136f8ff695
haebuki <40135144+haebuki@users.noreply.github.com>
2021-06-28 17:43:30 +0900

    Update note.py

--- basic/note.py
+++ basic/note.py
@@ -1,24 +1,35 @@
 #
-#  topic : 종이붙이기
+#  topic : parenthesis search 
 #  description : SW expert academy [string 학습] 문제 풀이
 #
 #  @author haebuki
-#  @date 2021-06-25
+#  @date 2021-06-8
 #  @version None 작업 내용 None
 #  @see None
 #
-f = [0 for _ in range(100)]
 test_case = int(input())
 
 for tc in range(1,test_case+1):
-    result=0
-    f[0]=1
-    f[1]=3
-    N = int(int(input())/10)
-
-    for n in range(2,N+1):
-        if f[n]==0:
-            f[n]=(2*f[n-2]+f[n-1])
-
-
-    print("#{} {}".format(tc, f[N-1]))
+	str = input()
+	result = 1
+	stack1=[]
+	for i in str:
+			if i == '(' or i=='{':
+				stack1.append(i)
+			elif i==')' or i=='}':
+				if stack1:
+					a=stack1.pop()	
+				else:
+					result=0
+					break	
+				if i==')' and a=='{':
+					result=0
+					break
+				if i=='}' and a=='(':
+					result=0
+					break
+					
+	if len(stack1)!=0:
+		result=0
+	
+	print("#{} {}".format(tc,result))
