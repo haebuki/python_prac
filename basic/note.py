a1d425550a2d0c655c3b39170deb4b0a927d151e
eutteum <ket5238@amtel.co.kr>
2021-06-29 17:40:53 +0900

    #sw academy 반복문자 지우기(History에 모든 문제풀이 있음)

--- basic/note.py
+++ basic/note.py
@@ -1,5 +1,5 @@
 #
-#  topic : 그래프 경로
+#  topic : 반복문자 지우기
 #  description : SW expert academy [string 학습] 문제 풀이
 #
 #  @author haebuki
@@ -11,32 +11,18 @@ test_case = int(input())
 
 for tc in range(1,test_case+1):
     result=0
-    stack1=[]
-    V,E = map(int, input().split())
-    map1 = [[0 for _ in range(V)] for _ in range(V)]
+    str = list(input())
+    stack1 = []
 
-    for _ in range(E):
-        X,Y = map(int, input().split())
-        map1[X-1][Y-1] = 1
-    S, G = map(int, input().split())
-    S -=1
-    G -=1
-    stack1.append(S)
+    for i in str:
+        if len(stack1) == 0:
+            pass
+        elif stack1[-1] == i:
+            stack1.pop()
+            continue
 
-    while 1:
-        a = stack1.pop()
-        for i in range(V):
-            #stack1에서 경로를 꺼내고 모든 노드의
-            #방향을 검사하고 이어져있는 경로를 스택에 저장
-            #찾고자하는 Y까지의 경로가 1이면 1 출력
-            if map1[a][G]==1:
-                result=1
-                break
-            if map1[a][i] == 1:
-                stack1.append(i)
-                continue
+        stack1.append(i)
 
-        if len(stack1)==0 or result==1:
-            break
+    result = len(stack1)
 
     print("#{} {}".format(tc, result))
